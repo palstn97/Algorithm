fa65bd79d7a55a0c621fb390a8c4a7e01b144862
palstn97 <palstn0624@gmail.com>
2025-04-06 00:11:19 +0900

    Update 분해합.py

--- 백준/Bronze/2231. 분해합/분해합.py
+++ 백준/Bronze/2231. 분해합/분해합.py
@@ -1,3 +1,4 @@
+# 뭐 속도면에서는 이게 더 좋은 풀이인듯
 N = int(input())
 
 lst = []
@@ -10,3 +11,15 @@ if lst:
     print(lst[0])
 else:
     print(0)
+
+# 이 방법이 더 느리다!
+# N = int(input())
+# 
+# lst = []
+# for i in range(1, N + 1):
+#     n_sum = i + sum(map(int, str(i)))
+#     if N == n_sum:
+#         print(i)
+#         break
+#     if i == N:
+#         print(0)
