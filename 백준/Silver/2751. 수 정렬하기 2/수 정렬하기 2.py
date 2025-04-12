1cafbf7f6c8f39c6cc5a3511fde1fedde3700a96
palstn97 <palstn0624@gmail.com>
2025-04-13 00:02:12 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 1108 ms, Memory: 78268 KB -BaekjoonHub

---
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.py
@@ -0,0 +1,13 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+lst = []
+
+for _ in range(N):
+    lst.append(int(input()))
+
+lst.sort()
+
+for num in lst:
+    sys.stdout.write(str(num) + '\n')
