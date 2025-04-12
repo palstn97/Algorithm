cafd6ec0ed68c03184ee5c2dc0c852c7233718a7
palstn97 <palstn0624@gmail.com>
2025-04-13 00:02:42 +0900

    Update 수 정렬하기 2.py

--- 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.py
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.py
@@ -1,13 +1,13 @@
 import sys
-input = sys.stdin.readline
+input = sys.stdin.readline  # 백준 문제는 입력을 이렇게 받아야한다고 한다,,,
 
 N = int(input())
 lst = []
 
 for _ in range(N):
-    lst.append(int(input()))
+    lst.append(int(input()))    # 입력 받은 수 lst에 append하기
 
 lst.sort()
 
 for num in lst:
-    sys.stdout.write(str(num) + '\n')
+    sys.stdout.write(str(num) + '\n')   # 출력 형식이다. 잘 알아두자.
