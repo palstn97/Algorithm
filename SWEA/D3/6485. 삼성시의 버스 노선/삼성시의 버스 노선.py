b2889f91feceb444dd7a76f97cef99d9e5efc69d
palstn97 <palstn0624@gmail.com>
2025-04-21 22:01:18 +0900

    Update 삼성시의 버스 노선.py

--- SWEA/D3/6485. 삼성시의 버스 노선/삼성시의 버스 노선.py
+++ SWEA/D3/6485. 삼성시의 버스 노선/삼성시의 버스 노선.py
@@ -16,3 +16,22 @@ for tc in range(1, T + 1):
                 count += 1
         lst.append(count)
     print(f"#{tc} {' '.join(map(str, lst))}")
+
+
+# 조금 더 좋은 풀이 -> 시간 단축 가능!
+T = int(input())
+
+for tc in range(1, T + 1):
+    N = int(input())
+    bus_list = [0]*5001
+    for i in range(N):
+        Ai, Bi = map(int, input().split())
+        for stop in range(Ai, Bi + 1):
+            bus_list[stop] += 1
+    P = int(input())
+    count = []
+    for j in range(P):
+        Cj = int(input())
+        count.append(bus_list[Cj])
+
+    print(f"#{tc} {' '.join(map(str, count))}")
