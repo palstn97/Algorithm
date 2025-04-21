9f4fb62d1af6f485cd2d49630abffd1b72292abc
palstn97 <palstn0624@gmail.com>
2025-04-22 01:21:15 +0900

    Update 어디에 단어가 들어갈 수 있을까.py

--- SWEA/D2/1979. 어디에 단어가 들어갈 수 있을까/어디에 단어가 들어갈 수 있을까.py
+++ SWEA/D2/1979. 어디에 단어가 들어갈 수 있을까/어디에 단어가 들어갈 수 있을까.py
@@ -26,3 +26,29 @@ for tc in range(1, T + 1):
     matrix = [list(map(int, input().split())) for _ in range(N)]
     result = word(matrix)
     print(f"#{tc} {result}")
+
+# 조금 더 괜찮은 풀이
+T = int(input())
+
+for tc in range(1, T + 1):
+    N, K = map(int, input().split())
+    matrix = [list(map(int, input().split())) for _ in range(N)]
+    result = 0
+    for c in range(N):
+        count = 0   # 문자열 길이 세기
+        # 행 우선
+        for r in range(N):
+            if matrix[c][r] == 1:
+                count += 1
+            if (matrix[c][r] == 0) or (r == N - 1): # 벽을 만나거나 0을 만나면 끝내는데
+                if count == K:  # 이 시점에서 count가 K라면
+                    result += 1 # result를 1 더해주기
+                count = 0   # 그리고 count 초기화
+        for r in range(N):
+            if matrix[r][c] == 1:   # 열 우선
+                count += 1
+            if matrix[r][c] == 0 or r == N - 1:
+                if count == K:
+                    result += 1
+                count = 0
+    print(f"#{tc} {result}")
