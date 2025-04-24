6c328f8db105929b95cdecd10f535892a39544b7
palstn97 <palstn0624@gmail.com>
2025-04-25 00:52:01 +0900

    Update 달팽이는 올라가고 싶다.py

--- 백준/Bronze/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.py
+++ 백준/Bronze/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.py
@@ -1,5 +1,17 @@
+# 1번 방법
 A, B, V = map(int, input().split())
 
 day = (V - B - 1) // (A - B) + 1
+
+print(day)
+
+# 2번 방법
+A, B, V = map(int, input().split())
+
+if (V - A) % (A - B) != 0 :
+    day = (V - A) // (A - B) + 1 + 1
+else:
+    day = (V - A) // (A - B) + 1
+
 
 print(day)
