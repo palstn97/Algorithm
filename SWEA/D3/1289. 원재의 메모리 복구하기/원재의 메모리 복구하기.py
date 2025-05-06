f613903c3da494da9dfda8064a746dcaea517034
palstn97 <palstn0624@gmail.com>
2025-05-06 16:16:06 +0900

    [D3] Title: 원재의 메모리 복구하기, Time: 68 ms, Memory: 58,880 KB -BaekjoonHub

--- SWEA/D3/1289. 원재의 메모리 복구하기/원재의 메모리 복구하기.py
+++ SWEA/D3/1289. 원재의 메모리 복구하기/원재의 메모리 복구하기.py
@@ -2,20 +2,13 @@ T = int(input())
 
 for tc in range(1, T + 1):
     N = input()
-    memory = []
+    cnt = 0
+    flag = False
     for i in N:
-        memory.append(i)
-    first = ['0'] * len(N)
-    count = 0
-    while first != memory:
-        for j in range(len(N)):
-            if memory[j] != first[j]:
-                if first[j] == '0':
-                    for k in range(j, len(N)):
-                        first[k] = '1'
-                    count += 1
-                else:
-                    for p in range(j, len(N)):
-                        first[p] = '0'
-                    count += 1
-    print(f"#{tc} {count}")
+        if flag and (i == '0'):
+            cnt += 1
+            flag = False
+        elif not flag and i == '1':
+            cnt += 1
+            flag = True
+    print(f"#{tc} {cnt}")
