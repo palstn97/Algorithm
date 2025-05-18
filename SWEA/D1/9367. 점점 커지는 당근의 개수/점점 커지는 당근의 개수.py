59d52dd47a7c52ad9baecbc7600b0dbb263e9861
palstn97 <palstn0624@gmail.com>
2025-05-18 15:11:02 +0900

    [D1] Title: 점점 커지는 당근의 개수, Time: 64 ms, Memory: 52,736 KB -BaekjoonHub

---
+++ SWEA/D1/9367. 점점 커지는 당근의 개수/점점 커지는 당근의 개수.py
@@ -0,0 +1,18 @@
+T = int(input())
+
+for tc in range(1, T + 1):
+    N = int(input())
+    arr = list(map(int, input().split()))
+
+    cnt = 1
+    max_cnt = 1
+    for i in range(N):
+        if i + 1 < N:
+            if arr[i] < arr[i + 1]:
+                cnt += 1
+                if max_cnt < cnt:
+                    max_cnt = cnt
+            else:
+                cnt = 1
+
+    print(f"#{tc} {max_cnt}")
