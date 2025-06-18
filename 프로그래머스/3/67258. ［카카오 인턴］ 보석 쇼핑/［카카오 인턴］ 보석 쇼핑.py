506807cc7dc86da5f8967d86138776dd67b076f4
palstn97 <palstn0624@gmail.com>
2025-06-18 22:56:26 +0900

    Update ［카카오 인턴］ 보석 쇼핑.py

--- 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.py
+++ 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.py
@@ -24,3 +24,38 @@ def solution(gems):
             l += 1  # 왼쪽 포인터 한 칸 옮기기
 
     return answer
+
+
+# 다른 방법
+def solution(gems):
+    answer = []
+    short = len(gems) + 1
+
+    l, r = 0, 0
+    jewel = len(set(gems))  # 보석 종류 수
+    jewel_cnt = 0           # 현재 윈도우 안의 보석 종류 수
+
+    dic = {}
+
+    while r < len(gems):
+        gem = gems[r]
+        if gem not in dic or dic[gem] == 0:
+            dic[gem] = 1
+            jewel_cnt += 1
+        else:
+            dic[gem] += 1
+        r += 1
+
+        while jewel_cnt == jewel:
+            if r - l < short:
+                short = r - l
+                answer = [l + 1, r]
+
+            dic[gems[l]] -= 1
+            if dic[gems[l]] == 0:
+                jewel_cnt -= 1
+                l += 1
+                break
+            l += 1
+
+    return answer
