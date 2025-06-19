e5b01791bb98429bc9e4afb86cf1edf8ee27df0e
palstn97 <palstn0624@gmail.com>
2025-06-19 16:36:28 +0900

    [Silver V] Title: 나이순 정렬, Time: 2656 ms, Memory: 46804 KB -BaekjoonHub

---
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.py
@@ -0,0 +1,14 @@
+N = int(input())
+
+member = []
+
+# key 매개변수를 가지는 sort() -> key값을 기준으로 정렬되고 기본값은 오름차순
+# lambda 식을 사용 할 수 있다. -> 그렇기에 두 개의 조건으로 정렬할 때는 이것을 사용
+
+for i in range(N):
+    age, name = input().split()
+    member.append((int(age), name))
+
+for j in sorted(member, key=lambda x: x[0]):
+    # 처음에는 member.sort(key=lambda x: x[0])으로 했는데 nonetype은 iterable하지 않다고 함
+    print(j[0], j[1])
