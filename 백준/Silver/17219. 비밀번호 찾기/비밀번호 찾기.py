ff4e20dffdea41f0021199be34fd1375c3752683
palstn97 <palstn0624@gmail.com>
2025-12-08 12:22:59 +0900

    [Silver IV] Title: 비밀번호 찾기, Time: 188 ms, Memory: 48452 KB -BaekjoonHub

--- 백준/Silver/17219. 비밀번호 찾기/비밀번호 찾기.py
+++ 백준/Silver/17219. 비밀번호 찾기/비밀번호 찾기.py
@@ -1,3 +1,6 @@
+import sys
+input = sys.stdin.readline
+
 N, M = map(int, input().split())
 # 2개의 정보니까 딕셔너리를 생각해야함
 password = {}
@@ -7,5 +10,5 @@ for i in range(N):
     password[site] = pw # 각각의 사이트에 맞게 비밀번호 딕셔너리에 저장
 
 for j in range(M):
-    site = input()
+    site = input().rstrip()
     print(password[site])
