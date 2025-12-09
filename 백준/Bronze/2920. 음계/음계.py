12ee19e14530b19cea4b76d224fa502a2353fa6c
palstn97 <palstn0624@gmail.com>
2025-12-09 10:47:57 +0900

    Update 음계.py

--- 백준/Bronze/2920. 음계/음계.py
+++ 백준/Bronze/2920. 음계/음계.py
@@ -13,4 +13,17 @@ if s == 7:
 elif s == -7:
     print("descending")
 else:
+
     print("mixed")
+
+'''
+이 풀이는 너무 직관적이지 못하다. 그래서 아래와 같은 풀이가 좀 더 직관적이고 괜찮을거 같다. 물론 효율 측면에서는 괜찮아 보이지만.
+music = list(map(int, input().split()))
+
+if music == [1, 2, 3, 4, 5, 6, 7, 8]:
+    print("ascending")
+elif music == [8, 7, 6, 5, 4, 3, 2, 1]:
+    print("descending")
+else:
+    print("mixed")
+'''
