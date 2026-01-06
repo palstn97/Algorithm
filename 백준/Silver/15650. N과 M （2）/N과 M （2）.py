11011ce5d6b5a4381839da374f2d9205664fcbea
palstn97 <palstn0624@gmail.com>
2026-01-06 14:43:20 +0900

    Update N과 M （2）.py

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.py
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.py
@@ -8,4 +8,31 @@ for i in range(1, N + 1):
 comb = combinations(lst, M)
 
 for c in comb:
+
     print(*c)
+
+
+'''
+from itertools import combinations
+
+N, M = map(int, input().split())
+
+# 1부터 N까지 리스트
+lst = list(range(1, N + 1))
+
+# combinations 객체 ([] 없이!)
+comb = combinations(lst, M)
+
+# 하나씩 꺼내서 출력
+for c in comb:
+    print(*c)  # * 붙여서!
+
+# 또는
+from itertools import combinations
+
+N, M = map(int, input().split())
+
+# 바로 for문
+for comb in combinations(range(1, N+1), M):
+    print(*comb)
+'''
