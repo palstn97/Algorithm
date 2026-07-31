19e25378d6425311035841411547544f464db573
palstn97 <palstn0624@gmail.com>
2026-07-31 21:40:43 +0900

    [level 1] Title: 덧칠하기, Time: 1.93 ms, Memory: 13.1 MB -BaekjoonHub

--- 프로그래머스/1/161989. 덧칠하기/덧칠하기.py
+++ 프로그래머스/1/161989. 덧칠하기/덧칠하기.py
@@ -1,4 +1,5 @@
 def solution(n, m, section):
+    # 시작 범위 설정하고 끝 범위 설정한 후 만약 section의 원소가 끝 범위보다 크다면 칠하는 횟수 1 증가시키고 범위 다시 설정하기
     answer = 1
     paint_start = section[0]
     paint_end = paint_start + m - 1
