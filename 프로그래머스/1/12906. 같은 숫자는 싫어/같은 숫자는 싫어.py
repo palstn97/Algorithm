f5b44678f988a7e711333c63b0fdb6812abc604f
palstn97 <palstn0624@gmail.com>
2026-08-05 00:11:09 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 156.47 ms, Memory: 29.6 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.py
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.py
@@ -6,7 +6,7 @@ def solution(arr):
     
     while q:
         now = q.popleft()
-        if now not in answer or now != answer[-1]:
+        if now not in answer or now != answer[-1]:  # 뒤에 answer에 담겨있는게 또 나올 수도 있기 때문에 가장 마지막 원소를 비교했을 때 다르다면 추가하는 방식으로 진행하면 된다.
             answer.append(now)
 
     return answer
