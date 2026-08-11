8d2490b3a195a4f7a6e4626e792eb99e4819daa8
palstn97 <palstn0624@gmail.com>
2026-08-11 09:56:46 +0900

    [level 1] Title: 햄버거 만들기, Time: 88.11 ms, Memory: 28 MB -BaekjoonHub

---
+++ 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.py
@@ -0,0 +1,12 @@
+def solution(ingredient):
+    stack = []
+    pattern = [1, 2, 3, 1]
+    answer = 0
+    
+    for i in ingredient:
+        stack.append(i)
+        if len(stack) >= 4 and stack[-4:] == pattern:
+            answer += 1
+            del stack[-4:]
+            
+    return answer
